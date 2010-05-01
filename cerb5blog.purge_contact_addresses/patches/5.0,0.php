1dbfee74bf71ff28d889697940cc099f0312350e
rmiddle <robert@middleswarth.net>
2010-05-01 15:08:30 -0400

    More tweaks to the 5.x patch.

--- cerb5blog.purge_contact_addresses/patches/5.0,0.php
+++ cerb5blog.purge_contact_addresses/patches/5.0,0.php
@@ -7,9 +7,7 @@ $tables = $db->metaTables();
 // ===========================================================================
 // Add index to message.address_id to make this purge works well.
 
-if(!isset($tables['message']))
-	return FALSE;
-	
+$logger->info("[Cerb5Blog.com] Creating index for message.address_id");
 list($columns, $indexes) = $db->metaTable('message');
 
 if(!isset($indexes['address_id'])) {
